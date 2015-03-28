708fa7d830b843eac3ffc4029d2d62f18f345ca8
uppfinnarn <uppfinnarn@gmail.com>
2015-03-28 12:35:00 +0100

    rails_admin

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+  
+  # Admin UI
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
