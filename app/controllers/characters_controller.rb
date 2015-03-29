ab50f67d885a6256a8452fae5c7243004857fff0
uppfinnarn <uppfinnarn@gmail.com>
2015-03-29 18:34:10 +0200

    Also don't let guests import characters

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -2,6 +2,8 @@ require 'securerandom'
 require 'open-uri'
 
 class CharactersController < ApplicationController
+  before_action :authenticate_user!, :except => :show
+  
   def import
     ensure_token
   end
