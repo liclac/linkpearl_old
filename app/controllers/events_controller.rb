5674bfb468933849ce8c83c745fefa4810db6c34
uppfinnarn <uppfinnarn@gmail.com>
2015-04-04 14:53:19 +0200

    You can now update events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,22 +1,29 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!, :except => :show
-  before_action :set_group
   load_and_authorize_resource
   
   def new
   end
   
   def create
+    @group = Group.find(params[:id])
     @event.group = @group
     @event.save
     redirect_to @group
   end
   
-  private
-  def set_group
-    @group = Group.find(params[:id])
+  def edit
   end
   
+  def update
+    if @event.update(event_params)
+      redirect_to @event.group
+    else
+      render 'edit'
+    end
+  end
+  
+  private
   def event_params
     params.require(:event).permit(:name, :time)
   end
