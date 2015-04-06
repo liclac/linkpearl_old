7b48f189966e43b131772a9d30d020861ee2e6e8
uppfinnarn <uppfinnarn@gmail.com>
2015-04-06 14:54:50 +0200

    Fixed swagger_docs initializer in production

--- config/initializers/swagger_docs.rb
+++ config/initializers/swagger_docs.rb
@@ -2,7 +2,7 @@ Swagger::Docs::Config.register_apis({
   "v1" => {
     :api_extension_type => :json,
     :api_file_path => "public/api/v1/",
-    :base_path => ("http://localhost:3000" if Rails.env.development?) + "/api/v1",
+    :base_path => ("http://localhost:3000" if Rails.env.development? else '') + "/api/v1",
     :clean_directory => false,
     :attributes => {
       :info => {
