b833c3ebfc641c2ec834bfac3d5fa521bb225b9f
uppfinnarn <uppfinnarn@gmail.com>
2015-04-06 06:44:39 -0700

    And fixed the 'Try it out' button

--- config/initializers/swagger_docs.rb
+++ config/initializers/swagger_docs.rb
@@ -1,9 +1,15 @@
+class Swagger::Docs::Config
+  def self.transform_path(path, api_version)
+    "/api/docs/#{api_version}/#{path}"
+  end
+end
+
 Swagger::Docs::Config.register_apis({
   "v1" => {
     :api_extension_type => :json,
-    :api_file_path => "public/api/v1/",
-    :base_path => (Rails.env.development? ? "http://localhost:3000" : '') + "/api/v1",
-    :clean_directory => false,
+    :api_file_path => "public/api/docs/v1/",
+    :base_path => ("http://localhost:3000" if Rails.env.development?),
+    :clean_directory => true,
     :attributes => {
       :info => {
         "title" => "API Documentation",
