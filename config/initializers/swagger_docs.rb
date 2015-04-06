06f7279336a15e3fd8ec04f739210363d21ef325
uppfinnarn <uppfinnarn@gmail.com>
2015-04-06 14:56:14 +0200

    Oops, that's Python syntax

--- config/initializers/swagger_docs.rb
+++ config/initializers/swagger_docs.rb
@@ -2,7 +2,7 @@ Swagger::Docs::Config.register_apis({
   "v1" => {
     :api_extension_type => :json,
     :api_file_path => "public/api/v1/",
-    :base_path => ("http://localhost:3000" if Rails.env.development? else '') + "/api/v1",
+    :base_path => (Rails.env.development? ? "http://localhost:3000" : '') + "/api/v1",
     :clean_directory => false,
     :attributes => {
       :info => {
