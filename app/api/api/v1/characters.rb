a9e83cd02dc70a5026a3d8a09ff34e6920bcc539
uppfinnarn <uppfinnarn@gmail.com>
2015-04-08 02:03:11 -0700

    /api/v1/characters/:id

--- app/api/api/v1/characters.rb
+++ app/api/api/v1/characters.rb
@@ -3,11 +3,22 @@ module API
     class Characters < Grape::API
       version 'v1'
       format :json
-
+      
+      helpers do
+        def get_character(id)
+          Character.where('id = ? or lodestone_id = ?', id, id).take!
+        end
+      end
+      
       resource :characters do
         desc "Return list of characters"
         get do
           Character.all
+        end
+        
+        desc "Returns a specific character"
+        get ':id' do
+          get_character(params[:id])
         end
       end
     end
