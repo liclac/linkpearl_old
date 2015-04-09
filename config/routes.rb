65f6c160d2d93490db5add55738b231133378744
uppfinnarn <uppfinnarn@gmail.com>
2015-04-09 11:37:44 +0200

    OAuth apps now require an owner

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  use_doorkeeper
+  use_doorkeeper do
+    controllers :applications => 'oauth/applications'
+  end
   devise_for :users
   
   # The priority is based upon order of creation: first created -> highest priority.
