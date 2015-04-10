2df0a1474b57ce0f1b389c9adf9a12e1daf8a1e1
uppfinnarn <uppfinnarn@gmail.com>
2015-04-10 06:57:08 -0700

    PUT on a group

--- app/api/api/v1/groups.rb
+++ app/api/api/v1/groups.rb
@@ -3,6 +3,13 @@ module API
     class Groups < Grape::API
       version 'v1'
       
+      helpers do
+        params :group_params do
+          optional :name, type: String
+          optional :message, type: String
+        end
+      end
+      
       resource :groups do
         desc "Returns all accessible groups" do
           success API::Entities::Group
@@ -24,12 +31,33 @@ module API
           ]
         end
         params do
-          requires :id, type: Integer, desc: "ID of the group"
+          requires :id, type: Integer
         end
         oauth2
         get ':id' do
           @group = Group.find(params[:id])
           authorize! :read, @group
+          present @group, with: API::Entities::Group
+        end
+        
+        desc "Updates group information" do
+          success API::Entities::Group
+          failure [
+            [404, "The group does not exist", API::Entities::Error],
+            [401, "Missing authentication", API::Entities::Error],
+            [403, "Not allowed to access this group", API::Entities::Error],
+          ]
+        end
+        params do
+          requires :id, type: Integer
+          use :group_params
+        end
+        oauth2
+        put ':id' do
+          puts params
+          @group = Group.find(params[:id])
+          @group.update(declared(params, include_missing: false))
+          authorize! :write, @group
           present @group, with: API::Entities::Group
         end
       end
