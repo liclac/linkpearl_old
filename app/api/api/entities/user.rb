c6878c5875de1cea6ccfcc005ca125cdd4daacf4
uppfinnarn <uppfinnarn@gmail.com>
2015-04-15 15:18:53 -0700

    Return the user's characters too

--- app/api/api/entities/user.rb
+++ app/api/api/entities/user.rb
@@ -5,10 +5,14 @@ module API
         type: Integer, required: true,
         desc: "Unique ID"
       }
-      
       expose :email, documentation: {
         type: String, required: true,
         desc: "Sign-in email"
+      }
+      
+      expose :characters, using: API::Entities::Character, documentation: {
+        is_array: true,
+        desc: "The user's characters"
       }
     end
   end
