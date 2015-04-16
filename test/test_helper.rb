e3cfb14b92eaf4322e54fb1ae6b0591e94437e21
uppfinnarn <uppfinnarn@gmail.com>
2015-04-16 05:59:09 -0700

    Oops, apparently you can't put them in fixtures
    ActiveRecord tries to load them as Database fixtures, not a good idea...

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,7 +6,7 @@ require 'vcr'
 require 'webmock/minitest'
 
 VCR.configure do |config|
-  config.cassette_library_dir = File.expand_path('../fixtures/vcr_cassettes', __FILE__)
+  config.cassette_library_dir = File.expand_path('../vcr_cassettes', __FILE__)
   config.hook_into :webmock
 end
 
