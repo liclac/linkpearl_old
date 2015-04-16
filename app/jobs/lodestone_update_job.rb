b612ee11e136e59a92c99bce35cdbc5267cb73d6
uppfinnarn <uppfinnarn@gmail.com>
2015-04-16 02:59:12 -0700

    Connection pooling for Lodestone pulls
    Resolves #14

--- app/jobs/lodestone_update_job.rb
+++ app/jobs/lodestone_update_job.rb
@@ -1,9 +1,13 @@
 class LodestoneUpdateJob < ActiveJob::Base
   queue_as :default
-
+  
+  CONNECTION_POOL = ConnectionPool.new(:size => 5, :timeout => 10) { Faraday.new }
+  
   def perform(thing, *args)
-    thing.lodestone_update(*args)
-    thing.synced_at = DateTime.now
-    thing.save!
+    CONNECTION_POOL.with do |connection|
+      thing.lodestone_update(*args, connection: connection)
+      thing.synced_at = DateTime.now
+      thing.save!
+    end
   end
 end
