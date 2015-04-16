93c0865d3fe7aa4b253128d5337f2e77cd73bd4b
uppfinnarn <uppfinnarn@gmail.com>
2015-04-16 06:15:36 -0700

    Tests for achievement pulls

--- test/models/character_test.rb
+++ test/models/character_test.rb
@@ -7,11 +7,22 @@ class CharacterTest < ActiveSupport::TestCase
   
   test "lodestone update" do
     VCR.use_cassette 'lodestone_emi' do
-      c = Character.find_by_lodestone_id(7248246)
+      c = Character.find_by_lodestone_id! 7248246
       c.lodestone_update
       assert_equal c.first_name, "Emi"
       assert_equal c.last_name, "Katapow"
       assert_equal c.bio, "25d0d67f057d46bdd4aea15bf16d4afa"
+    end
+  end
+  
+  test "lodestone update achievements" do
+    VCR.use_cassette 'lodestone_emi' do
+      c = Character.find_by_lodestone_id! 7248246
+      c.lodestone_update 'achievements'
+      assert_equal c.achievements.length, 244
+      
+      a = Achievement.find_by_name! "Green Eyes"
+      assert c.achievements.exists? a
     end
   end
 end
