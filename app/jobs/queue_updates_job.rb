6a2411c995d7d532332d4ceccf6bc0a9e973a260
uppfinnarn <uppfinnarn@gmail.com>
2015-04-16 04:23:30 -0700

    Oh, right, I don't need to do this anymore

--- app/jobs/queue_updates_job.rb
+++ app/jobs/queue_updates_job.rb
@@ -4,12 +4,10 @@ class QueueUpdatesJob < ActiveJob::Base
   def perform(class_name, *args)
     class_ = class_name.to_s.classify.constantize
     
-    offset = 0.minutes
     class_.find_in_batches(batch_size: 25) do |things|
       things.each do |thing|
-        LodestoneUpdateJob.set(wait: offset).perform_later(thing, *args)
+        LodestoneUpdateJob.perform_later(thing, *args)
       end
-      offset += 2.minutes
     end
   end
 end
