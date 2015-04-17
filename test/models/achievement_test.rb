cdf2839558f146260cee321c2b520b5c6c2144f2
uppfinnarn <uppfinnarn@gmail.com>
2015-04-17 02:05:13 -0700

    Unit tests for Achievements

--- test/models/achievement_test.rb
+++ test/models/achievement_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class AchievementTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "lodestone update" do
+    VCR.use_cassette 'lodestone_achievements' do
+      c = characters(:emi)
+      a = achievements(:green_eyes)
+      a.lodestone_update c.lodestone_id
+      assert_equal a.name, "Green Eyes"
+      assert_equal a.description, "Obtain the Veil of Wiyu in the quest \"A Relic Reborn.\""
+    end
+  end
 end
