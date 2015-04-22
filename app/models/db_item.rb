69764ca4eeaac9854a8b4de9569d3230ab18e46b
uppfinnarn <uppfinnarn@gmail.com>
2015-04-22 16:20:08 -0700

    I can pull item stats and stuff :D

--- app/models/db_item.rb
+++ app/models/db_item.rb
@@ -1,3 +1,69 @@
 class DBItem < ActiveRecord::Base
   belongs_to :category, class_name: 'DBItemCategory'
+  
+  include LodestoneLoadable
+  
+  def lodestone_update(**kwargs)
+    doc = lodestone_load **kwargs
+    extract_categories doc
+    extract_general doc
+    extract_stats doc
+  end
+  
+  def extract_categories(doc)
+    breadcrumbs = doc.css('#breadcrumb li > a')
+    
+    link1 = breadcrumbs[-2]
+    link2 = breadcrumbs[-1]
+    query = Rack::Utils.parse_query URI(link2.attr('href')).query
+    
+    cat1 = DBItemCategory.find_or_create_by! lodestone_id: query['category2'] do |c|
+      c.name = link1.text.strip
+    end
+    cat2 = DBItemCategory.find_or_create_by! lodestone_id: query['category3'] do |c|
+      c.name = link2.text.strip
+      c.parent = cat1
+      
+      attr_names = doc.css('.parameter_name')
+      c.attr1 = attr_names[0].text.strip if attr_names.length > 0
+      c.attr2 = attr_names[1].text.strip if attr_names.length > 1
+      c.attr3 = attr_names[2].text.strip if attr_names.length > 2
+    end
+    
+    self.category = cat2
+  end
+  
+  def extract_general(doc)
+    self.name = doc.css('.item_name').first.text.strip
+    self.description = doc.css('.eorzeadb_tooltip_mt5').first.try(:text).try(:strip)
+    self.unique = !doc.css('.rare').first.try(:text).blank?
+    self.untradable = !doc.css('.ex_bind').first.try(:text).blank?
+    
+    ilvl_e = doc.css('.eorzeadb_tooltip_pt3').first
+    self.ilvl = ilvl_e.text.sub('Item Level ', '').strip.to_i if ilvl_e
+    
+    classes_e = doc.css('.class_ok').first
+    self.classes = classes_e.text.strip if classes_e
+    
+    level_e = doc.css('.gear_level').first
+    self.level = level_e.text.sub('Lv. ', '').strip.to_i if level_e
+  end
+  
+  def extract_stats(doc)
+    attrs = doc.css('.sys_nq_element .parameter')
+    self.attr1 = attrs[0].text.strip.to_f if attrs.length > 0
+    self.attr2 = attrs[1].text.strip.to_f if attrs.length > 1
+    self.attr3 = attrs[2].text.strip.to_f if attrs.length > 2
+    
+    new_stats = []
+    doc.css('.sys_nq_element .basic_bonus li').each do |e|
+      stat, bonus = e.text.strip.split ' +', 2
+      new_stats.push [stat, bonus.to_i]
+    end
+    self.stats = new_stats
+  end
+  
+  def lodestone_link
+    "http://na.finalfantasyxiv.com/lodestone/playguide/db/item/#{lodestone_id}/"
+  end
 end
--- test/models/db_item_test.rb
+++ test/models/db_item_test.rb
@@ -1,7 +1,77 @@
 require 'test_helper'
 
 class DBItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "a unique weapon" do
+    VCR.use_cassette 'db_items/aiw_sword' do
+      i = DBItem.new lodestone_id: '695c329d011'
+      i.lodestone_update
+      
+      assert_equal "Arms", i.category.parent.name
+      assert_equal "Gladiator's Arm", i.category.name
+      assert_equal "Physical Damage", i.category.attr1
+      assert_equal "Auto-attack", i.category.attr2
+      assert_equal "Delay", i.category.attr3
+      
+      assert_equal "Augmented Ironworks Magitek Sword", i.name
+      assert_nil i.description
+      
+      assert i.unique
+      assert i.untradable
+      
+      assert_equal 130, i.ilvl
+      assert_equal 'GLA PLD', i.classes
+      assert_equal 50, i.level
+      
+      assert_equal 57, i.attr1
+      assert_equal 38.00, i.attr2
+      assert_equal 2.00, i.attr3
+      
+      assert_equal [
+        ['Strength', 38], ['Vitality', 45],
+        ['Accuracy', 25], ['Determination', 26],
+      ], i.stats
+    end
+  end
+  
+  test "a normal shield" do
+    VCR.use_cassette 'db_items/wootz_shield' do
+      i = DBItem.new lodestone_id: 'a0998dadcc1'
+      i.lodestone_update
+      
+      assert_equal "Armor", i.category.parent.name
+      assert_equal "Shield", i.category.name
+      assert_equal "Block Strength", i.category.attr1
+      assert_equal "Block Rate", i.category.attr2
+      assert_nil i.category.attr3
+      
+      assert_equal "Wootz Shield", i.name
+      assert_nil i.description
+      
+      assert_not i.unique
+      assert_not i.untradable
+      
+      assert_equal 110, i.ilvl
+      assert_equal 'GLA PLD', i.classes
+      assert_equal 50, i.level
+      
+      assert_equal 255, i.attr1
+      assert_equal 103, i.attr2
+      assert_nil i.attr3
+      
+      assert_equal [
+        ['Strength', 11], ['Vitality', 11],
+        ['Critical Hit Rate', 10], ['Skill Speed', 7],
+      ], i.stats
+    end
+  end
+  
+  test "a token item" do
+    VCR.use_cassette 'db_items/cs_atma' do
+      i = DBItem.new lodestone_id: '16cbb02efd1'
+      i.lodestone_update
+      
+      assert_equal "Atma of the Maiden", i.name
+      assert_equal "The crystallized essence of the fallen, won from a battle in the Central Shroud.", i.description
+    end
+  end
 end
